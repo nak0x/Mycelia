ee52348b81fcb27bec7049d1551e598fbe765a2a
Emmanuel Moulin <78955048+Kibishi47@users.noreply.github.com>
2025-12-14 18:00:41 +0100

    Feat: time_controller ask to developper to light on the next pixel

--- interaction-3/mycelium_developer/app/src/controller.py
+++ interaction-3/mycelium_developer/app/src/controller.py
@@ -1,15 +1,12 @@
 from framework.controller import Controller
-from framework.utils.frames.frame import Frame
+from framework.utils.frames.frame import Payload
 from framework.components.led_strip import LedStrip
 from framework.utils.gpio import GPIO
 
 class MainController(Controller):
 
     def setup(self):
-        self.led_strip = LedStrip(GPIO.GPIO27, 108, "mycelium_led_strip")
+        self.led_strip = LedStrip(GPIO.GPIO27, 108, "mycelium_led_strip", on_frame_received=self.mycelium_led_strip_frame_received)
 
-    def update(self):
-        pass
-
-    def on_frame_received(self, frame: Frame):
-        pass
+    def mycelium_led_strip_frame_received(self, led: LedStrip, payload: Payload):
+        led.next_pixel((255, 0, 0), show=True)
