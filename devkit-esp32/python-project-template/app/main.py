26ccec2bf5a4e7f480d1bd6a502065880f7d2245
Emmanuel Moulin <78955048+Kibishi47@users.noreply.github.com>
2025-12-14 13:51:50 +0100

    Feat: default working of led strip

--- devkit-esp32/python-project-template/app/main.py
+++ devkit-esp32/python-project-template/app/main.py
@@ -2,8 +2,9 @@ from src.app import App
 from src.utils.wifi import WifiManager
 from src.utils.integrity import run_integrity_checks
 from src.utils.ws.interface import WebsocketInterface
-from src.components.button import Button
 from src.utils.gpio import GPIO
+from src.components.button import Button
+from src.components.led_strip import LedStrip
 
 # Check that the esp32 don't have any problems
 run_integrity_checks()
@@ -20,6 +21,11 @@ button = Button(
     pin=GPIO.GPIO32,
     onPress=lambda: print("Button pressed"),
     onRelease=lambda: print("Button released")
+)
+
+led_strip = LedStrip(
+    pin=GPIO.GPIO25,
+    pixel_num=10
 )
 
 # Run the app
