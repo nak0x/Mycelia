88949f6ce21fc4e033349198a8a53c5078508f26
Emmanuel Moulin <78955048+Kibishi47@users.noreply.github.com>
2025-12-14 13:38:50 +0100

    Feat: command led using websocket

--- devkit-esp32/python-project-template/app/src/components/led.py
+++ devkit-esp32/python-project-template/app/src/components/led.py
@@ -1,11 +1,14 @@
 from src.app import App
 from machine import Pin
+from src.utils.frames.frame import Frame
 
 class Led:
     is_on = False
 
-    def __init__(self, pin):
+    def __init__(self, pin, slug = None):
         self.pin = Pin(pin, Pin.OUT, Pin.PULL_DOWN)
+        self.slug = slug
+        App().on_frame_received.append(self.on_frame_received)
 
     def on(self):
         self.pin.on()
@@ -14,3 +17,11 @@ class Led:
     def off(self):
         self.pin.off()
         self.is_on = False
+
+    def on_frame_received(self, frame: Frame):
+        if self.slug is None:
+            return
+        
+        for payload in frame.payload:
+            if payload.slug == self.slug and payload.datatype == "bool":
+                self.on() if payload.value else self.off()
