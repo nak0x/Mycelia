c07c9942c16d6dfd3bc2316f5ea415e2470fed23
Emmanuel Moulin <78955048+Kibishi47@users.noreply.github.com>
2025-12-15 16:29:58 +0100

    Feat: all experiment finished

--- interaction-3/nutrient/app/src/controller.py
+++ interaction-3/nutrient/app/src/controller.py
@@ -1,49 +1,55 @@
-import time
 from framework.controller import Controller
 from framework.utils.gpio import GPIO
 from framework.components.led_strip import LedStrip
+from framework.utils.timer import Timer
 from src.nurtient_flow import NutrientFlow
+from framework.utils.ws.interface import WebsocketInterface
 
 class MainController(Controller):
-    animated = False
+    animation_duration = 5000  # ms
+
     def setup(self):
         self.led_strip = LedStrip(GPIO.GPIO27, 100, "nutrient_led_strip")
         self.pixels = self.led_strip.pixels
 
-        # Paramètres réglables
         self.flow = NutrientFlow(
             num_pixels=len(self.pixels),
             color=(0, 255, 0),
-            wave_len=20,   # longueur d’un “paquet”
-            gap_len=15,   # leds éteintes entre deux paquets
-            speed=100.0,   # leds/seconde
+            wave_len=20,
+            gap_len=15,
+            speed=100.0,
             fade=True
         )
 
-        # (optionnel) limiter la fréquence d'affichage pour économiser CPU
-        self._frame_ms = 20  # 50 FPS max
-        self._t_last_frame = time.ticks_ms()
+        self.animated = False
+
+        # Timer qui stoppera l'animation après animation_duration
+        self.animation_timer = Timer(self.animation_duration, self.stop_animation)
 
     def update(self):
         if self.animated:
             self.handle_animation()
 
     def handle_animation(self):
-        # (optionnel) throttle
-        now = time.ticks_ms()
-        if time.ticks_diff(now, self._t_last_frame) < self._frame_ms:
-            return
-        self._t_last_frame = now
-
-        # 1) Met à jour le buffer
-        self.pixels = self.flow.step(self.pixels)
-
-        # 2) Push sur les neopixels
-        for i, c in enumerate(self.pixels):
-            self.led_strip.np[i] = c
+        self.flow.step(self.pixels)
         self.led_strip.display()
+
+    def start_animation(self):
+        self.animated = True
+        self.animation_timer.restart()
+
+    def stop_animation(self):
+        self.animated = False
+        # optionnel: clear strip à la fin
+        for i in range(len(self.pixels)):
+            self.pixels[i] = (0, 0, 0)
+        self.led_strip.display()
+        WebsocketInterface().send_value("shroom_growing", True, "bool", "ESP32-030301")
 
     def on_frame_received(self, frame):
         for payload in frame.payload:
-            if payload.slug == "animated" and payload.datatype == "bool":
-                self.animated = payload.value
+            if payload.slug == "nutrient_animated" and payload.datatype == "bool":
+                if payload.value:
+                    self.start_animation()
+                else:
+                    self.stop_animation()
