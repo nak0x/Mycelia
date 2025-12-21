93e9180f12852aaa52bca610af023cb26f38361c
Emmanuel Moulin <78955048+Kibishi47@users.noreply.github.com>
2025-12-21 09:58:31 +0100

    Feat: update framework & use encoder

--- interaction-3/mycelium_time_controller/app/src/controller.py
+++ interaction-3/mycelium_time_controller/app/src/controller.py
@@ -1,11 +1,12 @@
 from framework.controller import Controller
-from framework.components.button import Button
+from framework.components.encoder import Encoder
 from framework.utils.gpio import GPIO
 from framework.utils.ws.interface import WebsocketInterface
 
 class MainController(Controller):
     def setup(self):
-        Button(GPIO.GPIO32, onPress=self.increment_mycelium)
+        Encoder(pinA=GPIO.GPIO25, pinB=GPIO.GPIO26, onCw=self.increment_mycelium)
 
     def increment_mycelium(self):
-        WebsocketInterface().send_value("mycelium_led", True, "bool", "ESP32-030102")
+        print("Incrementing mycelium")
+        WebsocketInterface().send_value("mycelium_deployment", True, "bool", "ESP32-030102")
