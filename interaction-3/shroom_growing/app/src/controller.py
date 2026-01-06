4a7f34a2f881272275e306814a075b2895236924
Emmanuel Moulin <78955048+Kibishi47@users.noreply.github.com>
2026-01-06 09:49:14 +0100

    Feat: turn cw or ccw an engine

--- interaction-3/shroom_growing/app/src/controller.py
+++ interaction-3/shroom_growing/app/src/controller.py
@@ -5,4 +5,18 @@ from framework.components.relay import Relay
 class MainController(Controller):
 
     def setup(self):
-        self.relay = Relay(GPIO.GPIO27)
+        self.r1 = Relay(GPIO.GPIO27)
+        self.r2 = Relay(GPIO.GPIO26)
+
+    def on_frame_received(self, frame):
+        if frame.action == "3-engine-cw":
+            self.r1.open()
+            self.r2.close()
+        elif frame.action == "3-engine-ccw":
+            self.r1.close()
+            self.r2.open()
+        elif frame.action == "3-engine-off":
+            self.r1.close()
+            self.r2.close()
+        elif frame.action == "3-start-animation":
+            pass
