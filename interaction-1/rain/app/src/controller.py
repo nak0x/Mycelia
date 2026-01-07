6f4f2bf048e38004d8ea8bc5b6974fd13af0787e
Nak0_x <nak0x@proton.me>
2026-01-07 22:28:09 +0100

    Apdapte to local server + pickup de humidity sensor

--- interaction-1/rain/app/src/controller.py
+++ interaction-1/rain/app/src/controller.py
@@ -1,11 +1,15 @@
 from framework.controller import Controller
-from framework.components.relay import Relay
+from framework.components.dht_sensor import DHTSensor
+from framework.utils.gpio import GPIO
 
 
 class RainController(Controller):
     
     def __init__(self):
         super().__init__()
-        relay = Relay(27, "01-rain-toggle")
-        relay.close()
+        relay = DHTSensor(GPIO.GPIO14, onHumidityChange=self.handle_humidity)
+
+    def handle_humidity(self, humidity):
+        print(humidity)
+        
 
