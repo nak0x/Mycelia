dd75d7400b79d081f2300a9d45de8f167e93e8ec
Emmanuel Moulin <78955048+Kibishi47@users.noreply.github.com>
2026-01-08 10:11:26 +0100

    Feat: handle client connection using their id

--- devkit/python-server-template/app/ws_controllers/core.py
+++ devkit/python-server-template/app/ws_controllers/core.py
@@ -2,6 +2,7 @@ import json
 from aiohttp import web
 from app.ws_controllers.base import WsController
 from app.frames.frame import Frame
+from typing import List
 import asyncio
 
 
@@ -21,9 +22,20 @@ class CoreController(WsController):
         self._interaction_2_done = False
 
     async def on_ping(self, frame: Frame, ws: web.WebSocketResponse) -> None:
-        sender = frame.metadata.get("senderId", "UNKNOWN")
-        print("[WS] PING received from", sender)
+        print("[WS] PING received from", frame.sender_id)
         await ws.send_str(json.dumps(self.build_frame("pong", "pong")))
+
+    async def on_new_connection(self, frame: Frame, ws: web.WebSocketResponse) -> None:
+        await self.hub.set_client(frame.sender_id, ws)
+
+    async def on_get_connected_clients(self, frame: Frame, ws: web.WebSocketResponse) -> None:
+        data: List[dict] = []
+        for id, client in self.hub._setted_clients.items():
+            data.append({
+                "clientId": id,
+                "isConnected": client is not None
+            })
+        await ws.send_str(json.dumps(self.build_frame("connected-clients", data)))
 
     # interaction 1
     async def on_shroom_forest_lighten(self, frame: Frame, ws: web.WebSocketResponse) -> None:
